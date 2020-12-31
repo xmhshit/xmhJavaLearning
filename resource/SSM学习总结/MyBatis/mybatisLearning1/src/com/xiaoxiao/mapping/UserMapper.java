b7f3066edbae00992e83f88ecb4940744343dc1f
Menghan Xiao <1073870993@qq.com>
2020-12-31 18:05:42 +0800

    关于Mybatis的学习总结

---
+++ resource/SSM学习总结/MyBatis/mybatisLearning1/src/com/xiaoxiao/mapping/UserMapper.java
@@ -0,0 +1,10 @@
+//package com.xiaoxiao.mapping;
+//
+//import org.apache.ibatis.annotations.Select;
+//
+//import com.xiaoxiao.domain.User;
+//
+//public interface UserMapper {
+//	@Select("select * from loginSystem where id = #{id}\r\n") 
+//	public User selectUser(int id);
+//}
